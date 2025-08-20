b97f5d867d074b0d517b6f30d99a1ee9c4eb622d
Naomi-Faffy <faffynaomi@gmail.com>
2025-08-20 14:43:49 +0200

    Updated homepage design and fixed bugs

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Thread Theory Creators</h1>
+      </header>
+      <main>
+        <p>Welcome to the Thread Theory creators page!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
